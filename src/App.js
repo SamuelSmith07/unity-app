a59363acd6974858704f82971cdcccd7a4fc9e42
SamuelSmith07 <samsmith1@live.com>
2022-07-16 13:29:48 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import Unity_Technologies_logo from './Unity_Technologies_logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import React, { Component } from 'react';
 
-function App() {
+export default function App() {
   return (
+    
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <Box
+      sx={{
+        position: 'static',
+        display: "flex",
+        width: '75%',
+        height: '300',
+        backgroundColor: '#2D2D2D',
+        borderRadius: '45px',
+      }}>
+      <img src={Unity_Technologies_logo} className="App-logo" alt="Unity Logo" />
+        <h1>Recipe Book</h1>
+        <Box
+      sx={{
+        position: 'absolute',
+        backgroundColor: '#1D1D1D',
+        borderRadius: '45px',
+      }}></Box>
+      <Button>Save</Button>
+      </Box>
+        <Button variant="contained">Hello World</Button>
       </header>
+      
     </div>
+    
   );
 }
 
-export default App;
+
